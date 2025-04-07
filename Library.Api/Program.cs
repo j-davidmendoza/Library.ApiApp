8d3c2a932841b6ce1de438a9b212e618e6d95184
David Mendoza <mendoza.d.jesus@gmail.com>
2025-04-07 11:03:57 -0700

    Adding Validation

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+using FluentValidation.Results;
 using Library.Api.Data;
 using Library.Api.Models;
 using Library.Api.Services;
@@ -17,19 +19,34 @@ builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
 
 builder.Services.AddSingleton<DatabaseInitializer>();
 
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>(); // This will register all the validors in the assembly
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapPost("books", async (Book book, IBookService bookService) =>
+app.MapPost("books", async (Book book, IBookService bookService,
+    IValidator<Book> validator) =>
 {
+    var validationResult = await validator.ValidateAsync(book);
+    if (!validationResult.IsValid)
+    {
+        //return Results.ValidationProblem();
+        return Results.BadRequest(validationResult.Errors); //You may You might not want to expose to your consumers the validator result obejct and you might wanna have yourt own contract, you would do the mapping for that contract here 
+    }
+
     var created = await bookService.CreateAsync(book);
     if(!created)
     {
-        return Results.BadRequest(new
+        //return Results.BadRequest(new
+        //{
+        //    errorMessage = "A book with the same ISBN-13 already exists or invalid data provided.",
+        //});
+        return Results.BadRequest(new List<ValidationFailure>
         {
-            errorMessage = "A book with the same ISBN-13 already exists or invalid data provided.",
+            new ValidationFailure("Isbn", "A book with the same ISBN-13 already exists or invalid data provided.")
         });
     }
 
