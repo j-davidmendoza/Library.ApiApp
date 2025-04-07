f13a4c412b818b70df8bb48a88cf304afe7b6651
David Mendoza <mendoza.d.jesus@gmail.com>
2025-04-06 22:45:12 -0700

    Project start up and database creation

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,7 +1,31 @@
+using Library.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
+//    new SqliteConnectionFactory("hardCodedConectionString"));
+builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
+    new SqliteConnectionFactory
+    (builder.Configuration.GetValue<string>("Database:ConnectionString")));
+
+builder.Services.AddSingleton<DatabaseInitializer>();
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+
 app.MapGet("/", () => "Hello World!");
+
+
+// Db init here
+var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
+await databaseInitializer.IntializeAsync();
 
 app.Run();
 
