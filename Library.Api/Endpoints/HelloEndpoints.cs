93c0f63e1e152018cc300b0c951f8a7800e20a8a
David Mendoza <mendoza.d.jesus@gmail.com>
2025-04-11 10:27:59 -0700

    Testing dymanic endpoint additions

---
+++ Library.Api/Endpoints/HelloEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using Library.Api.Endpoints.Internal;
+
+namespace Library.Api.Endpoints;
+
+public class HelloEndpoints : IEndpoints
+{
+    public static void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+       
+    }
+
+    public static void DefineEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("helloAdded", () => "Hello World from newly added endpoint");
+    }
+}
