13a9e9e55e8658b7638e9e7fef2c420c7e01c201
David Mendoza <mendoza.d.jesus@gmail.com>
2025-04-12 15:06:49 -0700

    Testing resource creation

--- Library.Api.Tests.Integration/LibraryEndpointTests.cs
+++ Library.Api.Tests.Integration/LibraryEndpointTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using FluentAssertions;
+using FluentValidation.Results;
+using Library.Api.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
 
 namespace Library.Api.Tests.Integration;
 
@@ -11,6 +15,88 @@ public class LibraryEndpointTests: IClassFixture<WebApplicationFactory<IApiMarke
         _factory = factory;
     }
 
+    [Fact]
+    public async Task CreateBook_CreatesBook_WhenDataIsCorrect()
+    {
+        //Arrange
+        var httpClient = _factory.CreateClient();
+        var book = GenerateBook();
+
+        //Act
+        var result = await httpClient.PostAsJsonAsync("/books", book);
+        var createdBook = await result.Content.ReadFromJsonAsync<Book>();
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
+        createdBook.Should().BeEquivalentTo(book);
+        result.Headers.Location.Should().Be($"/books/{book.Isbn}");
+
+    }
+
+    [Fact]
+    public async Task CreateBook_Fails_WhenIsbnIsInvalid()
+    {
+        //Arrange
+        var httpClient = _factory.CreateClient();
+        var book = GenerateBook();
+        book.Isbn = "invalid-isbn";
+
+
+        //Act
+        var result = await httpClient.PostAsJsonAsync("/books", book);
+        var errors = await result.Content.ReadFromJsonAsync<IEnumerable<ValidationError>>();
+        var error = errors!.Single();
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        error.PropertyName.Should().Be("Isbn");
+        error.ErrorMessage.Should().Be("Value was not valid ISBN-13");
+
+
+    }
+
+
+    [Fact]
+    public async Task CreateBook_Fails_WhenBookExists()
+    {
+        //Arrange
+        var httpClient = _factory.CreateClient();
+        var book = GenerateBook();
+
+        //Act
+        await httpClient.PostAsJsonAsync("/books", book);
+        var result = await httpClient.PostAsJsonAsync("/books", book);
+        var errors = await result.Content.ReadFromJsonAsync<IEnumerable<ValidationError>>();
+        var error = errors!.Single();
+
+        //Assert
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        error.PropertyName.Should().Be("Isbn");
+        error.ErrorMessage.Should().Be("A book with the same ISBN-13 already exists or invalid data provided.");
+
+
+    }
+
+
+    private Book GenerateBook(string title = "The Dirty Coder")
+    {
+        return new Book
+        {
+            Isbn = GenerateIsbn(),
+            Title = title,
+            Author = "David Mendoza",
+            PageCount = 420,
+            ShortDescription = "All my tricks in one book",
+            ReleaseDate = new DateTime(2026, 10, 1)
+        };
+    }
+
+    private string GenerateIsbn()
+    {
+        return $"{Random.Shared.Next(100, 999)}-" +
+               $"{Random.Shared.Next(1000000000, 2100999999)}";
+    }
+
 
 
 
@@ -19,7 +105,7 @@ public class LibraryEndpointTests: IClassFixture<WebApplicationFactory<IApiMarke
     //public void Test()
     //{
     //    var httpClient = _factory.CreateClient();
-        
+
     //}
     //You dont need this all, instead do the above, with an implementation of IClassFixture
     ////You can seperate these methods to seperate classes but we'll keep them together for simplicity
